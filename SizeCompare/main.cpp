7b242debbc3b22b1020227b4b49563395788614e
Matt Cleere <mattcleere@gmail.com>
2020-09-30 13:24:51 -0700

    WILL THIS SHIT EVER WORK!!!

---
+++ SizeCompare/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SizeCompare.cpp
+ *
+ * Created: 9/30/2020 12:24:24 PM
+ * Author : mattc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
